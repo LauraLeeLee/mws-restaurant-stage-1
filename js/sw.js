129e3c07958c17d350f3db78e820cf882a46cd53
Laura Logan <laurardh@aol.com>
2018-05-24 16:08:41 -0400

    adds sw.js file and code

---
+++ js/sw.js
@@ -0,0 +1,63 @@
+var appCacheName = 'restaurant-review-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(appCacheName).then(function(cache) {
+      return cache.addAll([
+        '/skeleton',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg',
+        'css/styles.css'
+        '//normalize-css.googlecode.com/svn/trunk/normalize.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheVersions) {
+      return Promise.all(
+        cacheVersions.filter(function(cacheVersion) {
+          return cacheVersion.startsWith('restaurant-') && cacheVersion != appCacheName;
+        }).map(function(cacheVersion) {
+          return caches.delete(cacheVersion);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if(requestUrl.origin === location.origin) {
+    if(requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if(event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
